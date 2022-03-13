76e486dc3107f0d3195f6bf9d8dd1dbbdaba5626
rouwe <santosroweme14@gmail.com>
2022-03-13 17:13:09 +0800

    Header interactivity for mobile and desktop

--- js/header.js
+++ js/header.js
@@ -10,6 +10,81 @@ function setOuterHeaderWidth() {
     } else {
         outerHeader.style.width = '100%';
     }
-    console.log(innerHeaderWidth)
 };
 setInterval(setOuterHeaderWidth, 100);
+
+const closeMenuButton = document.getElementsByClassName('header-close-menu')[0];
+const openMenuButton = document.getElementsByClassName('header-open-menu')[0];
+const outerHeader = document.getElementsByClassName('header-container')[0];
+const screenWidth = window.innerWidth;
+if (screenWidth > 767) {
+    // Menu Event for tablet and up
+    openMenuButton.addEventListener('click', desktopOpenMenu);
+    closeMenuButton.addEventListener('click', desktopCloseMenu);
+} else {
+    // Menu Event for mobile
+    openMenuButton.addEventListener('click', mobileOpenMenu);
+    closeMenuButton.addEventListener('click', mobileCloseMenu); 
+}
+
+const innerHeader = document.getElementsByClassName('inner-header-container')[0];
+const navMenuBox = document.getElementsByClassName('nav-menu-box')[0];
+const menuBox = document.getElementsByClassName('menu-box')[0];
+const navName = document.getElementsByClassName('nav-name')[0];
+const navCraft = document.getElementsByClassName('nav-craft')[0];
+const navDataBox = document.getElementsByClassName('nav-data-box')[0];
+const navItemBox = document.getElementsByClassName('nav-item-box');
+const navQuote = document.getElementsByClassName('nav-quote')[0];
+const navDestinations = document.getElementsByClassName('nav-destination');
+function desktopOpenMenu() {
+    // Opens the navigation menu for tablet devices and up
+    closeMenuButton.style.display = 'flex';
+    openMenuButton.style.display = 'none';
+    navName.style.display = 'block';
+    navCraft.style.display = 'block';
+    navQuote.style.display = 'block';
+    for (const navDestination of navDestinations) {
+        navDestination.style.display = 'block';
+    }
+    navDataBox.style.flexDirection = 'row';
+    navDataBox.style.rowGap = '0px';
+    menuBox.style.alignItems = 'flex-start';
+    for (const navItem of navItemBox) {
+        navItem.style.justifyContent = 'flex-start';
+    }
+    
+}
+function desktopCloseMenu() {
+    // Closes the navigation menu for tablet devices and up
+    closeMenuButton.style.display = 'none';
+    openMenuButton.style.display = 'flex';
+    navName.style.display = 'none';
+    navCraft.style.display = 'none';
+    navQuote.style.display = 'none';
+    for (const navDestination of navDestinations) {
+        navDestination.style.display = 'none';
+    }
+    navDataBox.style.flexDirection = 'column';
+    navDataBox.style.rowGap = '0.8rem';
+    menuBox.style.alignItems = 'center';
+    for (const navItem of navItemBox) {
+        navItem.style.justifyContent = 'center';
+    }
+}
+function mobileOpenMenu() {
+    // Open mobile menu
+    openMenuButton.style.display = 'none';
+    closeMenuButton.style.display = 'flex';
+    innerHeader.style.maxHeight = 'unset';
+    navDataBox.style.display = 'none';
+    navMenuBox.style.display = 'flex';
+}
+function mobileCloseMenu() {
+    // Close mobile menu
+    const headerMaxHeight = menuBox.offsetHeight;
+    openMenuButton.style.display = 'flex';
+    closeMenuButton.style.display = 'none';
+    innerHeader.style.maxHeight = `${headerMaxHeight}px`;
+    navDataBox.style.display = 'none';
+    navMenuBox.style.display = 'flex';
+}
